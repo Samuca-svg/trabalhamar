18f06a500f2a7186ac5a75dfb839c1938af9754b
Samuel E <sprocopio06@gmail.com>
2025-06-23 19:54:37 -0300

    primeiro commit

---
+++ teste_marciano.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h> 
+
+int main() {
+    int i = 0;
+    while (1) {
+        printf("\rCarregando");
+        for (int j = 0; j < i % 4; j++) printf(".");
+        fflush(stdout);
+        i++;
+        usleep(300000); 
+    }
+    return 0;
+}
